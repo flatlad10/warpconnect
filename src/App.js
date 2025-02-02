721aab59be10f9b9a17e01bdbf764de120adcf23
Anil Tayal <aniltayal@Anils-Mac-mini.local>
2025-02-02 17:46:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import '@farcaster/auth-kit/styles.css';
+import { AuthKitProvider } from '@farcaster/auth-kit';
+import { SignInButton, useProfile} from '@farcaster/auth-kit';
+
+const config = {
+  rpcUrl: 'https://mainnet.optimism.io',
+  domain: 'example.com',
+  siweUri: 'https://example.com/login',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main style={{ fontFamily: 'Inter, "Inter Placeholder", sans-serif' }}>
+      {/* @ts-expect-error ethers version type incompat */}
+      <AuthKitProvider config={config}>
+        <div style={{ position: "fixed", top: "12px", right: "12px" }}>
+          <SignInButton />
+        </div>
+        <div style={{ paddingTop: "33vh", textAlign: "center" }}>
+          
+          <Profile />
+          
+        </div>
+      </AuthKitProvider>
+    </main>
+  );
+}
+
+function Profile() {
+  const profile = useProfile();
+  const {
+    isAuthenticated,
+    profile: { fid, displayName, custody },
+  } = profile;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <div>
+          <p>
+            Hello, {displayName}! Your FID is {fid}.
+          </p>
+          <p>
+            Your custody address is: <pre>{custody}</pre>
+          </p>
+        </div>
+      ) : (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click the "Sign in with Farcaster" button above, then scan the QR code
+          to sign in.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      )}
+    </>
   );
 }
 
